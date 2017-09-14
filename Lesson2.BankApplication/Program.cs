22e1193ff0b0ebb497f0a5bb2863b339ff371c63
Andrey Filipev <a.filipev@dodopizza.com>
2017-09-14 18:54:11 +0300

    Add question about name

--- Lesson2.BankApplication/Program.cs
+++ Lesson2.BankApplication/Program.cs
@@ -7,6 +7,13 @@ namespace Lesson2.BankApplication
 	{
 		public static void Main(string[] args)
 		{
+			Console.WriteLine("Hello!\r\n What's your name?");
+
+			string name = Console.ReadLine();
+			
+			Console.WriteLine("Name: {0}", name);
+
+			Console.ReadLine();
 		}
 	}
 }
