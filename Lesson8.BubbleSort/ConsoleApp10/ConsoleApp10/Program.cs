32a3249f7dd5d3bbb994726ff3d3130978549aea
Andrey Filipyev <a.filipev@dodopizza.com>
2019-11-22 15:54:04 +0300

    add Lessong 8

---
+++ Lesson8.BubbleSort/ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random r = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(100);
+                Console.Write(array[i] + " ");
+            }
+
+            BubbleSort(array);
+
+            Console.WriteLine();
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+
+
+        public static void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length-1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        array[j] = array[j] + array[j + 1];
+                        array[j + 1] = array[j] - array[j + 1];
+                        array[j] = array[j] - array[j + 1];
+                    }
+                }
+            }
+        }
+    }
+}
